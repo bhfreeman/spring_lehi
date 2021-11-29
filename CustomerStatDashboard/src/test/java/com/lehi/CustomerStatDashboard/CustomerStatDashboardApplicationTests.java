e7282b571a476e0c6cbfc80e8c18c247a584a679
bhfreeman <brett.h.freeman@gmail.com>
2021-11-28 19:21:12 -0700

    init commit, spring.io and create-react-app

---
+++ CustomerStatDashboard/src/test/java/com/lehi/CustomerStatDashboard/CustomerStatDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lehi.CustomerStatDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerStatDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
